715205290b431bb6b813e12a34ade892bce4470f
workingjubilee <workingjubilee@gmail.com>
2019-01-29 11:18:51 -0800

    Initial commit.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 // Your code goes here
+// * [ ] Using your [index.js file](js/index.js), create 10 [unique event listeners](https://developer.mozilla.org/en-US/docs/Web/Events). using your creativity to make the Fun Bus site more interactive.  Here are some unique events you could try to use:
+
+// * [ ] `mouseover`
+// * [ ] `keydown`
+// * [ ] `wheel`
+// * [ ] `drag / drop`
+// * [ ] `load`
+// * [ ] `focus`
+// * [ ] `resize`
+// * [ ] `scroll`
+// * [ ] `select`
+// * [ ] `dblclick`
+
+// * [ ] Nest two similar events somewhere in the site and prevent the event propagation properly
+// * [ ] Stop the navigation from items from refreshing the page by using `preventDefault()`
+
+
+// ## Stretch Task:
+//
+// * [ ] Go look at [GSAP](https://greensock.com/) and implement the animations found in that library with your custom events.
+//
+//
+//
+// ## Stretch assignment
+//
+// * [ ] Take a look at the [stretch assignment](stretch-assignment) and follow the instructions in the read me file.
